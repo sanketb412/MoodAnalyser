b94291852bb6da8d906a76ae560f30c965aa1248
sanketb412 <sanketbagde38@gmail.com>
2021-05-16 17:32:48 +0530

    Added: code to return Happy from given Message

--- src/test/java/com/moodException.java
+++ src/test/java/com/moodException.java
@@ -4,10 +4,24 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class moodException {
+
+    //*
+    // Added message of sad mood
+    //***
     @Test
     public void giveMessage_WhenSad_ShouldReturnSad() {
         MoodAnalyser analyser = new MoodAnalyser();
-        String mood = analyser.analyseMood("i am in Sad Mood");
-        Assertions.assertEquals("SAD",mood);
+        String mood = analyser.analyseMood("i am in Sad Mood"); //Message
+        Assertions.assertEquals("SAD",mood); //excepted sad and return sad
+    }
+    
+    //*
+    // Added message of happy mood
+    //***
+    @Test
+    public void giveMessage_WhenHappy_ShouldReturnHappy() {
+        MoodAnalyser analyser = new MoodAnalyser();
+        String mood = analyser.analyseMood("i am in Happy Mood"); //Message
+        Assertions.assertEquals("HAPPY",mood); //excepted happy and return happy
     }
 }
